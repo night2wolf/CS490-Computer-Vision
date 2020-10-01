1b4f99420bf648e400c411781d2cf1d85ed28c3e
Night2wolf <trevor.klinkenberg@gmail.com>
2020-10-01 14:01:18 -0500

    What I have

--- CS490HW2/HW2.py
+++ CS490HW2/HW2.py
@@ -3,20 +3,25 @@ NOTE for Grader to get vlfeat:
 Needs 3.6 python environment and numpy and cython.
 then run:
 conda install -c menpo cyvlfeat
+Additional NOTE:
+Feature aggregation is library found here:
+https://github.com/paschalidoud/feature-aggregation 
+run setup.py install as administrator
+
 References:
 https://github.com/menpo/cyvlfeat
 https://github.com/Lithogenous/VLAD-SIFT-python/blob/master/vlad_raw.py
 https://gist.github.com/danoneata/9927923 
-https://github.com/menpo/cyvlfeat/blob/master/cyvlfeat/sift/dsift.py
-https://github.com/menpo/cyvlfeat/blob/master/cyvlfeat/sift/sift.py 
 https://github.com/Vectorized/Python-KD-Tree/blob/master/kdtree.py
 """
-from cyvlfeat import vlad,fisher,kmeans,sift
+from cyvlfeat import vlad,fisher,kmeans,sift,gmm
 import cv2
-import sklearn
-import HW2helpers
+from sklearn import metrics,svm,pipeline
 import scipy
 import numpy as np
+import matplotlib.pyplot as plt
+from feature_aggregation import FisherVectors,Vlad,BagOfWords
+import glob
 #Q1
 """
 Compute Image Features, create a matlab/python function that compute n x d features by calling
@@ -29,7 +34,7 @@ implementing the following function:
 # NOTE - im must be A single channel, greyscale, `float32` numpy array (ndarray)
 #  representing the image to calculate descriptors for for sift and dsift
 def getImageFeatures(im, opt):
-    
+    print(im)
     im =cv2.imread(im)    
     img_gray= cv2.cvtColor(im,cv2.COLOR_BGR2GRAY)
     if opt == "dsift" or opt == "sift":
@@ -60,7 +65,8 @@ def getVladModel(f, kd, k):
     print(centers)
     kdtree = scipy.spatial.KDTree(centers)    
     #knn = scipy.spatial.KDTree.query(kdtree,1)
-    vlad_km = vlad.vlad(f,centers,kdtree)        
+    vlad_km = vlad.vlad(f,centers,kdtree)
+           
     return vlad_km
 """  -- Do the same for Fisher Vector model """
 # f - n x d matrix containing n features from say 100 images. 
@@ -69,53 +75,62 @@ def getVladModel(f, kd, k):
 # fv_gmm - FisherVector GMM model:
 #         fv_gmm.m - mean, fv_gmm.cov - variance, fv_gmm.p - prior
 # A - PCA for dimension reduction
-def getFisherVectorModel(f, kd, k):
-    fisher_km = fisher.fisher()
+def getFisherVectorModel(f, kd, k):    
+    fv_gmm = gmm.gmm(f,n_clusters=k)
+    print(fv_gmm)        
+    fv_gmmM= fv_gmm[0]
+    fv_gmmC = fv_gmm[1]
+    fv_gmmP = fv_gmm[2]
+    print(fv_gmmC)
+    print(fv_gmmP)      
+    #fisher_km =""
+    fisher_km = fisher.fisher(f,fv_gmmM,fv_gmmC,fv_gmmP)
     return fisher_km
 
 
-#Q3
-"""
-Compute VLAD and Fisher Vector Aggregation of Images,
-from the given VLAD and FV models, implementing the following functions.
-Notice that the feature nxd f need to be projected to the desired lower dimension via,
-f0=f*A(:,1:kd), to match the VLAD model dimension before calling this function. 
-"""
-# f - n x d matrix containing a feature from an image by calling f=getImageFeature(im,.. 
-# vlad_km - VLAD kmeans model
-def getVladAggregation(vlad_km, f):
-    return
-""" --Do the same for Fisher Vector mdoel """
-# f - n x d matrix containing a feature from an image by calling f=getImageFeature(im,.. 
-# fv_gmm - GMM Model from features, has m, cov, and p. 
-def getFisherVectorAggregation(fv_gmm, f):
-    return
-
-
-#Q4
-"""
-Now benchmarking the TPR-FPR performance of various feature and aggregation scheme performance against
-the mini CDVS data set. For the SIFT and DenseSIFT features, let us have kd=[24, 48], nc=[32, 64, 96].
-So for each image, we will have 2 x 6 x2  = 24 different feature + aggregation representations.
-For the total of N images in the mini CDVS dataset, we have M=N*(N-1)/2 total image pairs,
-and the matching pairs ground truth are given, we only care about the first 100 matching pairs and
-first 100 non-matching pairs in the fid.mat, which has two variables mp and nmp.
-Each has a row of two image filenames to their associated images, e.g, mp(1,:): mp_2.jpg and mp1_2.jpg
-are two matching pairs: 
-And nmp(1,:) contains file names for the following non-matching pairs 
-Let us use the Euclidean distance to on those features to compute the TPR-FPR ROCs
-and find out which one have the best performance. Last year’s plots are attached below for example,
-you only need to plot the last row for 10 feature-aggregation combinations.
-"""    
-kd = [24,48]
-nc = [32,64,96]
+#Q3 & Q4  
 im = "MPEG.CDVS\cdvs_thumbnails\cdvs-11.jpg"
-frames = getImageFeatures(im,opt="sift")
-print(frames)
-#vlad_km = getVladModel(frames,kd,10)
-#print(vlad_km)
-frames,descriptor = getImageFeatures(im,opt="dsift")
-print(frames)
-print(descriptor)
-vlad_km = getVladModel(frames,kd,1)
+imgs = "MPEG.CDVS\cdvs_thumbnails"
+# Gather all features in an array
+dsftfeatures = [
+    getImageFeatures(imge,opt="dsift")
+    for imge in glob.glob("MPEG.CDVS\cdvs_thumbnails" + "/*.jpg")    
+]
+
+sftfeatures = [
+    getImageFeatures(imge,opt="sift")
+    for imge in glob.glob("MPEG.CDVS\cdvs_thumbnails" + "/*.jpg")    
+]
+# Fit Vlad and Fisher Vector Models based on Sift and Dsift Features
+sfttrain = np.arange(len(sftfeatures))
+dsfttrain = np.arange(len(dsftfeatures))
+#Shuffle the data set for training and test batches
+np.random.shuffle(sfttrain)
+np.random.shuffle(dsfttrain)
+sfttest = sfttrain[100:]
+dsfttest = dsfttrain[100:]
+sfttrain = sfttrain[:100]
+dsfttrain = dsfttrain[:100]
+# Create Fisher Vector and Vlad Models then fit them on the data (see Feature_Aggregation for functions)
+sftfv = FisherVectors(100)
+sftfv.fit(sftfeatures)
+sftfv.transform(sftfeatures)
+sftvlad = Vlad(100)
+sftvlad.fit(sftfeatures)
+sftvlad.transform(sftfeatures)
+dsftfv = FisherVectors(100)
+dsftfv.fit(dsftfeatures)
+dsftfv.transform(dsftfeatures)
+dsftvlad = Vlad(100)
+dsftvlad.fit(sftfeatures)
+dsftvlad.transform(sftfeatures)
+
+"""
+vlad_km = getVladModel(features,kd,24)
 print(vlad_km)
+fisher_km = getFisherVectorModel(features,kd,24)
+print(fisher_km)
+"""
+#final measurement roc curve use this:
+# https://scikit-learn.org/stable/modules/classes.html#sklearn-metrics-metrics
+#https://scikit-learn.org/stable/modules/generated/sklearn.metrics.plot_roc_curve.html#sklearn.metrics.plot_roc_curve
